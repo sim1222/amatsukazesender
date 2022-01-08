878caf42754f0d8231beaf9e3051b08ade38ff7c
sim1222 <kokt@sim1222.com>
2022-01-09 02:37:27 +0900

    プロジェクト ファイルを追加します。

---
+++ amatsukazesender/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace amatsukazesender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
