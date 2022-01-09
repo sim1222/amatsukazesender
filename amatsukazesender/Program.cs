93f174d0557d99d0b82f29e5b91c35896053632c
sim1222 <kokt@sim1222.com>
2022-01-10 02:15:47 +0900

    とりあえずできた

--- amatsukazesender/Program.cs
+++ amatsukazesender/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using Oika.Libs.CuiCommandParser;
+using System.Configuration;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
 
 namespace amatsukazesender
 {
@@ -27,6 +31,7 @@ namespace amatsukazesender
             usagebuilder.Summary = "TSファイルのパスを置き換えたあとに、AmatsukazeにAddTaskします";
 
             usagebuilder.AddUseCase(usagebuilder.NewUseCase().AddArg(usagebuilder.NewUseCaseArg("OPTION").AsMultiple().AsOptional())
+                                                             .AddArg(usagebuilder.NewUseCaseArg("TSFile"))
                                                              .AddArg(usagebuilder.NewUseCaseArg("BeforeSTR"))
                                                              .AddArg(usagebuilder.NewUseCaseArg("AfterSTR"))
                                                              .AddArg(usagebuilder.NewUseCaseArg("Amatsukaze Path").AsOptional()));
@@ -48,12 +53,92 @@ namespace amatsukazesender
             Console.WriteLine(usagebuilder.ToString());
             // initおわり
 
+            string prebefval = "C:\\";
+            string preaftval = "F:\\";
+            string preamtpath = "C:\\Amatsukaze_0.9.1.4\\Amatsukaze\\exe_files\\AmatsukazeAddTask.exe";
 
 
+            // String teststr = "C:\\TV\\afffafs\\saafsf!!!!!##nya.mp4";
+            // Console.WriteLine("Original: {0}", teststr);
+            // Console.WriteLine("Replace: {0}", teststr.Replace(prebefval, preaftval));
 
 
+            var opbefval = parsed.GetOptionValue('b');
+            var opaftval = parsed.GetOptionValue('a');
+            var opamtpath = parsed.GetOptionValue('p');
 
 
+            var suffix = parsed.CommandParameters;
+            string befval = (parsed.HasOption('b') ? opbefval : prebefval);
+            string aftval = (parsed.HasOption('a') ? opaftval : preaftval);
+            string amtpath = (parsed.HasOption('p') ? opamtpath : preamtpath);
+
+            
+            if (suffix[0].EndsWith(".ts") == false)
+            {
+                Console.WriteLine("これはTSファイルではありません");
+                return;
+            }
+
+            string amtopt = (" -r \"G:\friio\\Amatsukaze_0.9.1.4\\Amatsukaze\" -ip \"192.168.1.3\" -p 32768 -o \"F:\\TV\\encoded\" -s \"x265 - 10Bitインタレ解除アニメ用（CUDA必須）\" --priority 3 --no-move" + "-f \"" + suffix[0].Replace(befval, aftval) + "\"");
+
+            string amtcommand = (amtpath + amtopt);
+            
+            Console.WriteLine("元ファイル: {0}", suffix[0]);
+            Console.WriteLine();
+            Console.WriteLine("コピー先環境パス: {0}", suffix[0].Replace(befval, aftval));
+            Console.WriteLine();
+            Console.WriteLine("Amatsukazeパス: {0}", amtpath);
+            Console.WriteLine();
+            Console.WriteLine("Amatsukaze実行コマンド: {0}", amtcommand);
+            Console.WriteLine();
+
+            // メイン処理
+
+            int retrynum = 10000;
+            int retrycount = 0;
+
+            Console.WriteLine("ファイルコピーを開始します");
+            while (true)
+            {
+                try
+                {
+                    File.Copy(suffix[0], suffix[0].Replace(befval, aftval), false);
+                }
+                catch (IOException error)
+                {
+                    Console.WriteLine(error.Message);
+                    if (retrycount++ < retrynum)
+                    {
+                        Console.WriteLine("エラーが発生しました: {0}", error);
+                        Console.WriteLine("5秒後に再試行します… リトライ回数: {0}", retrycount);
+                        Thread.Sleep(5);
+                    } else
+                    {
+                        Console.WriteLine("リトライ回数が(0)に達しました。処理を中止します。", retrycount);
+                        Console.WriteLine("エラー内容: {0}", error);
+                        throw error;
+                    }
+                }
+            }
+            Console.WriteLine("コピーが完了しました。");
+
+            Console.WriteLine("Amatsukazeにタスクを追加します");
+
+            var amtproc = new Process();
+
+            amtproc.StartInfo.FileName = amtpath;
+            amtproc.StartInfo.UseShellExecute = true;
+            amtproc.StartInfo.Arguments = amtopt;
+            amtproc.Start();
+
+            amtproc.WaitForExit();
+
+            Console.WriteLine("多分完了しました。");
+            Console.WriteLine("元TSを削除します");
+            File.Delete(suffix[0]);
+
+            return;
 
         }
     }
